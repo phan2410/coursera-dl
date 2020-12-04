0435b343275c681f96713acb4bec413f9da00fb3
phan2410 <phan2410@gmail.com>
2020-12-04 11:40:55 +0700

    fix path bug in cmd

--- transfer/dl-course.sh
+++ transfer/dl-course.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-../coursera-dl -c cookies.txt --video-resolution 720p --path /home/myuser/dump --subtitle-language en $1
+./coursera-dl -c cookies.txt --video-resolution 720p --path /home/myuser/dump --subtitle-language en $1
